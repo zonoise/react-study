4ffe5735cf37366ca64c3979858dbdefa08e6a22
zonoise <zonoise@gmail.com>
2021-05-05 03:06:34 +0900

    lightsをlocalStorageに保存

--- src/Area.tsx
+++ src/Area.tsx
@@ -12,32 +12,43 @@ interface LightProps {
     checked:boolean;
 }
 
+const AREA_KEY = "AREA";
+
 function Area(){
+
     const [lights, setLights] = useState<LightProps[]>([]);
 
     useEffect(() => {
-        const lightList:LightProps[] = [
-            {
-                id:1,
-                lat:1,
-                long:1,
-                checked:true
-            },
-            {
-                id:2,
-                lat:2,
-                long:2,
-                checked:false
-            },
-            {
-                id:3,
-                lat:3,
-                long:3,
-                checked:false
-            }
-        ];
-        //todo fetch webapi ??  
-        setLights(lightList);
+
+        const areaData = localStorage.getItem(AREA_KEY);
+        let initialState:LightProps[]=[];
+        if(null !== areaData){
+            initialState = JSON.parse(areaData);
+            setLights(initialState);
+        }else{
+            const lightList:LightProps[] = [
+                {
+                    id:1,
+                    lat:1,
+                    long:1,
+                    checked:true
+                },
+                {
+                    id:2,
+                    lat:2,
+                    long:2,
+                    checked:false
+                },
+                {
+                    id:3,
+                    lat:3,
+                    long:3,
+                    checked:false
+                }
+            ];
+            //todo fetch webapi ??  
+            setLights(lightList);
+        } 
     }, [])
 
     const handleClick = useCallback((targetId) => {
@@ -45,13 +56,17 @@ function Area(){
         const index = lights.findIndex(light => light.id === targetId);
         
         const light = lights[index];
-        light.checked = true;
+        light.checked = !light.checked ;
         
         const newlights = [...lights];
         newlights.splice(index, 1,light);
         setLights(newlights);
 
     }, [lights]);
+
+    useEffect(() => {
+        localStorage.setItem(AREA_KEY, JSON.stringify(lights))
+    }, [lights])
 
     return (
         <div className="Area">
